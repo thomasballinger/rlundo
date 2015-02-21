01a673c47b7b446f1af42d65c8f7cf9f0da68ca0
Thomas Ballinger <thomasballinger@gmail.com>
2015-02-21 09:29:52 -0500

    ctl-d EOF works

--- rlundoable.c
+++ rlundoable.c
@@ -18,7 +18,8 @@
 
 #include <string.h>
 
-char *last_command = "with no command, so exiting";
+char *top_undo_message = "with no command, so exiting";
+char *last_command = NULL;
 
 /* Read a line of input.  Prompt with PROMPT.  An empty PROMPT means
    none.  A return value of NULL means that EOF was encountered. */
@@ -26,20 +27,30 @@ char* readline(const char *prompt){
 
   char *value;
 
-  char *(*original_readline)(const char*)=NULL;
+  if (!last_command){
+    last_command = malloc(strlen(top_undo_message) + 1);
+    strcpy(last_command, top_undo_message);
+  }
+  char *(*original_readline)(const char*) = NULL;
   if (!original_readline){
-    original_readline = dlsym(RTLD_NEXT,"readline");
+    original_readline = dlsym(RTLD_NEXT, "readline");
   }
 
   value = (*original_readline)(prompt);
+  if(!value){
+    free(last_command);
+    printf("\n");
+    exit(0);
+  }
   if(!strcmp(value, "undo")){
     printf("undoing '%s'\n", last_command);
+    free(last_command);
     exit(42);
   }
    pid_t pid = fork();
 
   if (pid == 0) { // child
-    last_command = value;
+    last_command = strdup(value);
     return value;
   } else { // parent
     int status;
@@ -56,14 +67,14 @@ char* readline(const char *prompt){
 
 int main(){
    char* line;
-   int a = 0;
+   int depth = 0;
 
    while (1){
-     a++;
+     depth++;
      line = readline("enter a string: ");  // readline allocates space for returned string
      if(line != NULL) { 
          printf("You entered: %s\n", line);
-         printf("a is now: %d\n", a);
+         printf("depth: %d\n", depth);
          free(line);   // but you are responsible for freeing the space
      }
    }
