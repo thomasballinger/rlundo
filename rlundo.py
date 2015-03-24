36916a3b47821959c7a1cf7b24366bd612091739
Thomas Ballinger <thomasballinger@gmail.com>
2015-03-24 19:24:19 +0000

    using strdup, apparently patch wasn't working

--- rlundo.py
+++ rlundo.py
@@ -8,7 +8,9 @@ modified_readline_path = os.path.join(os.path.dirname(os.path.realpath('__file__
 if sys.platform == 'darwin':
     os.environ["DYLD_LIBRARY_PATH"] = modified_readline_path
 else:
-    os.environ["LD_LIBRARY_PATH"] = modified_readline_path
+    os.environ["LD_LIBRARY_PATH"] = ':'.join([modified_readline_path])
+    os.environ["LD_PRELOAD"] = '/lib/x86_64-linux-gnu/libtinfo.so.5'
+
 
 p = Popen(sys.argv[1:])
 p.communicate()
