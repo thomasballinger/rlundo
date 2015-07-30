9b1d00a00b759178a385e0eecba9d5eec438e0f3
Madelyn Freed <madelyn.freed@gmail.com>
2015-07-30 12:42:45 -0400

    refactored start_undoable to use argparse

--- rlundo/__main__.py
+++ rlundo/__main__.py
@@ -9,6 +9,7 @@ Start a repl with undo feature.
 from __future__ import unicode_literals
 import sys
 import os
+import argparse
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import rlundo
@@ -19,14 +20,18 @@ from rlundo.termrewrite import run_with_listeners
 from rlundo import interps
 
 
-def start_undoable_rl(args):
+def start_undoable_rl(interpreter, interparg):
     for command, predicate in interps.interpreters:
-        if predicate(args[1]):
-            return run_with_listeners(command + args[2:])
+        if predicate(interpreter):
+            return run_with_listeners(command + interparg)
     else:
         modify_env_with_modified_rl()
-        run_with_listeners(args[1:])
+        run_with_listeners(interpreter + interparg)
 
 
 if __name__ == "__main__":
-    start_undoable_rl(sys.argv)
+    parser = argparse.ArgumentParser(description='accepting an interpreter and any interpreter arguments into rlundo')
+    parser.add_argument('interpreter', metavar='I', help='command to call the interpreter')
+    parser.add_argument('interparg', nargs=argparse.REMAINDER, help='any arguments you can feed into the interpreter')
+    argobject = parser.parse_args()
+    start_undoable_rl(argobject.interpreter, argobject.interparg)
