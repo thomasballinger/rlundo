daed90c36c25bf64e2987c16e15f60bf0c6e23e4
Thomas Ballinger <thomasballinger@gmail.com>
2016-09-08 22:06:44 -0400

    try 2

--- .travis.tmux.sh
+++ .travis.tmux.sh
@@ -2,10 +2,10 @@
 set -e
 set -x
 
-curl https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz > tmux.tar.gz
-tar -czvf tmux.tar.gz tmuxbuild
-cd tmuxbuild
+wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
+tar xzvf tmux-2.2.tar.gz
+cd tmux-2.2
 ./configure
-./make
+make
 chmod +x tmux
 sudo mv tmux /usr/local/bin
